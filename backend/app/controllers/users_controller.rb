a06deb5ddf581a8ad75bd9a31e706e04e9fe4081
Igor Veyner <igorveyner95@gmail.com>
2021-01-21 14:38:03 -0500

    add strict params to user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def create
-    user = User.create(name: params[:username])
+    user = User.create(name: user_params)
     render json: user
+  end
+
+  private
+
+  def user_params
+    params.require(:username)
   end
 end
