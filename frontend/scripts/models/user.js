1aa8832727fa41c5b8d8f6b478ffd82aeb6e922a
Igor Veyner <igorveyner95@gmail.com>
2021-01-21 14:40:30 -0500

    update comments

--- frontend/scripts/models/user.js
+++ frontend/scripts/models/user.js
@@ -6,9 +6,9 @@ class User {
   }
 
   // TODO
-  // 2. User instance create an instance of login display component
-  //      this component render the form for User creation
-  // 3. create a name setter that accepts an argument
+  // 1. Add a method to create a game-display component
+  //      it will display all the html elements associated with the game
+  // 2. create a name setter that accepts an argument
   //      it will set this.id & this.name
   //      no real function to this but in the future if I were to add features to
   //      edit / delete users this would be useful information
