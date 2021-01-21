91fedbf96b7a2b61b773d79ee55cbb68c4fdaa62
Igor Veyner <igorveyner95@gmail.com>
2021-01-21 16:23:46 -0500

    add username to game display

--- frontend/scripts/components/game-display.js
+++ frontend/scripts/components/game-display.js
@@ -2,5 +2,20 @@ class GameDisplay {
   constructor(user) {
     this.user = user
     this.main = document.querySelector("main")
+    this.createDisplay()
+  }
+
+  createDisplay = () => {
+    this.addUsername()
+  }
+
+  addUsername = () => {
+    const elementsToCreate = ["div", "h2"]
+    const [usernameContainer, username] = elementsToCreate.map(element => document.createElement(element))
+
+    username.innerText = this.user.username
+
+    usernameContainer.appendChild(username)
+    this.main.appendChild(usernameContainer)
   }
 }
