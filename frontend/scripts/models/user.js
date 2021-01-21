7b9f4c896963d75ec7d8bb32f0253e7962ba05aa
Igor Veyner <igorveyner95@gmail.com>
2021-01-21 16:41:08 -0500

    remove old todo comments

--- frontend/scripts/models/user.js
+++ frontend/scripts/models/user.js
@@ -5,17 +5,10 @@ class User {
     this.display = new LoginForm(this)
   }
 
-  // This does nothing functionally atm
-  // If I add edit username / delete account this will become useful
-
   updateInfo = data => {
     this.id = data.id
     this.username = data.name
   }
 
   displayGame = () => this.display = new GameDisplay(this)
-
-  // TODO
-  // 1. Add a method to create a game-display component
-  //      it will display all the html elements associated with the game
 }
