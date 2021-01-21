27399a08a3948f275f4fbbe1719f2b0165e96400
Igor Veyner <igorveyner95@gmail.com>
2021-01-21 13:09:59 -0500

    setup basic postUser fetch

--- frontend/scripts/services/api.js
+++ frontend/scripts/services/api.js
@@ -3,7 +3,18 @@ class API {
     this.url = `http://localhost:${port}`
   }
 
+  headers = {"Accepts": "application/json", "Content-Type": "application/json"}
+
   get userURL() {
     return this.url + '/users'
+  }
+
+  postUser = () => {
+    return fetch(this.userURL, {
+      method: "POST",
+      headers = this.headers,
+      body: JSON.stringify({})
+    })
+    .then(console.log)
   }
 }
