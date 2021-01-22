120fc37130b18a15d947a20b0e9d7b0640cd4114
Igor Veyner <igorveyner95@gmail.com>
2021-01-22 17:53:50 -0500

    add getCurrentScore func to handle logic of getting and converting score data

--- frontend/scripts/models/score.js
+++ frontend/scripts/models/score.js
@@ -17,5 +17,17 @@ class Score {
     // Add helper to get the current time / strikes / check if LOC is done
   }
 
+  getCurrentScore = () => {
+    // TODO: 
+    // 1. add method to check if line of code is complete
+    let timer = this.user.display.scoreDiv.timer.innerText
+    let strikes = this.user.display.scoreDiv.num.innerText
+
+    timer = parseInt(timer.replace(":", ""), 10) * 10
+    strikes = parseInt(strikes)    
+
+    return [timer, strikes]
+  }
+
   assignID = scoreData => this.id = scoreData.id 
 }
