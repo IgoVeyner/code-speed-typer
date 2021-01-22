a2d2f2452ebbdb83a5b26e7f5fef6e04e0a7fb20
Igor Veyner <igorveyner95@gmail.com>
2021-01-22 15:10:25 -0500

    add show action

--- backend/app/controllers/scores_controller.rb
+++ backend/app/controllers/scores_controller.rb
@@ -1,10 +1,24 @@
 class ScoresController < ApplicationController
+  before_action :find_score, only: [:show]
+
   def create
     score = Score.create(score_params)
     render json: score
   end
 
+  def show
+    if @score
+      render json: @score
+    else
+      render json: {error: "Sorry, there is no score with that ID", status: 400}, status: 400
+    end
+  end
+
   private
+
+  def find_score
+    @score = Score.find_by_id(params[:id])
+  end
 
   def score_params
     params.require(:score).permit(:user_id)
