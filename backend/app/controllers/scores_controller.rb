9b625b0e984ce44e589ede3e2c605841e27976cb
Igor Veyner <igorveyner95@gmail.com>
2021-01-22 14:56:59 -0500

    add create action and strict params

--- backend/app/controllers/scores_controller.rb
+++ backend/app/controllers/scores_controller.rb
@@ -1,2 +1,12 @@
 class ScoresController < ApplicationController
+  def create
+    score = Score.create(score_params)
+    render json: score
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:user_id)
+  end
 end
