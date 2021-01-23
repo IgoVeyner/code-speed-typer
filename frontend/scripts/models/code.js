231de61899406fd0add905d106da163299edbfe9
Igor Veyner <igorveyner95@gmail.com>
2021-01-23 13:11:47 -0500

    update code class to assign its attributes from DB data after fetch request

--- frontend/scripts/models/code.js
+++ frontend/scripts/models/code.js
@@ -7,7 +7,14 @@ class Code {
   // 1. update instance info from fetch request
   // 2. update func to get random code data
 
-   getCode = () => {
-    this.user.api.fetchCode(1)
+  getCode = () => {
+    this.user.api.fetchCode(4)
+    .then(this.handleData)
+  }
+
+  handleData = codeData => {
+    this.id = codeData.id
+    this.line = codeData.line
+    this.name = codeData.line
   }
 }
