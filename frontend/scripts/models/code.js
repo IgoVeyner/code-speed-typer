2b28b7e932809b6c1b5ec933ee2a40388ba15bfe
Igor Veyner <igorveyner95@gmail.com>
2021-01-23 13:07:06 -0500

    add helper to do tell the api to do a fetch request

--- frontend/scripts/models/code.js
+++ frontend/scripts/models/code.js
@@ -2,4 +2,12 @@ class Code {
   constructor(user) {
     this.user = user
   }
+
+  // TODO:
+  // 1. update instance info from fetch request
+  // 2. update func to get random code data
+
+   getCode = () => {
+    this.user.api.fetchCode(1)
+  }
 }
