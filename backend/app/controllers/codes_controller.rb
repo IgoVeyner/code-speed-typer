b6acf0e22ef8730c37af2e5b5bdbdc9fa8cb66ae
Igor Veyner <igorveyner95@gmail.com>
2021-01-23 12:45:58 -0500

    add show action

--- backend/app/controllers/codes_controller.rb
+++ backend/app/controllers/codes_controller.rb
@@ -1,2 +1,9 @@
 class CodesController < ApplicationController
+  def show
+    if @code
+      render json: @code
+    else
+      render json: {error: "Sorry, there is no code with that ID", status: 400}, status: 400
+    end
+  end
 end
