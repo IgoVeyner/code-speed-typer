539815aef26f3cbe77dfead9b217f864f8107463
Igor Veyner <igorveyner95@gmail.com>
2021-01-23 12:47:36 -0500

    add find_code helper and use before action

--- backend/app/controllers/codes_controller.rb
+++ backend/app/controllers/codes_controller.rb
@@ -1,9 +1,17 @@
 class CodesController < ApplicationController
+  before_action :find_code, only:[:show, :update]
+
   def show
     if @code
       render json: @code
     else
       render json: {error: "Sorry, there is no code with that ID", status: 400}, status: 400
     end
+  end
+
+  private
+
+  def find_code
+    @code = Code.find_by_id(params[:id])
   end
 end
