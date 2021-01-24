4e621b76d96ea4df8b2c94025229f6a132d78e41
Igor Veyner <igorveyner95@gmail.com>
2021-01-24 15:50:45 -0500

    add comment explaining functionality of handle data

--- frontend/scripts/models/code.js
+++ frontend/scripts/models/code.js
@@ -13,6 +13,11 @@ class Code {
     this.id = codeData.id
     this.line = codeData.line
     this.name = codeData.line
+
+    // Create Score needs to run after the above data is assigned,
+    //  otherwise score creation will not have a code_id to send
+    //  and it does not get created in the DB
+
     this.user.createScore()
     this.user.createGameDisplay()
   }
