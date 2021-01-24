e88d5a04cff48a73788fc865437eadc8b39f4787
Igor Veyner <igorveyner95@gmail.com>
2021-01-24 17:05:23 -0500

    add regular seeds back

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,6 +2,8 @@ User.destroy_all
 Score.destroy_all
 Code.destroy_all
 
-Code.create(name: "", line: "a")
-# Code.create(name: "Map", line: "a.map.with_index {|x,i| x * i}")
-# Code.create(name: "Compact", line: "[\"a\", nil, \"b\", nil, \"c\", nil].compact")
+# Here for quick testing purposes
+# Code.create(name: "", line: "a")
+
+Code.create(name: "Map", line: "a.map.with_index {|x,i| x * i}")
+Code.create(name: "Compact", line: "[\"a\", nil, \"b\", nil, \"c\", nil].compact")
