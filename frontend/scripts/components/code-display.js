09110bc022b4a8f35c25ae58a69184420433bb3a
Igor Veyner <igorveyner95@gmail.com>
2021-01-24 11:36:08 -0500

    update component to set it's user, main and line attributes and create dom elements

--- frontend/scripts/components/code-display.js
+++ frontend/scripts/components/code-display.js
@@ -1,6 +1,18 @@
 class CodeDisplay {
-  constructor(user, line) {
+  constructor(main, user) {
+    this.main = main
     this.user = user
-    this.line = line
+    this.line = this.user.code.line
+    this.createDisplay()
+  }
+
+  createDisplay = () => {
+    const div = document.createElement("div")
+    for (const char of this.line){
+      const span = document.createElement("span")
+      span.innerText = char
+      div.appendChild(span)
+    }
+    this.main.appendChild(div)
   }
 }
