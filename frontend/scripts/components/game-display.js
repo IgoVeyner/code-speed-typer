3338a9febb0428947bb74af2668469e237d9b880
Igor Veyner <igorveyner95@gmail.com>
2021-01-24 11:46:41 -0500

    remove code abstracted to component

--- frontend/scripts/components/game-display.js
+++ frontend/scripts/components/game-display.js
@@ -9,27 +9,6 @@ class GameDisplay {
     this.nameplate = new Nameplate(this.main, this.user)
     this.scoreDiv = new ScoreDisplay(this.main, this.user)
     this.codeDisplay = new CodeDisplay(this.main, this.user)
-    this.addUserInput()
-  }
-
-  addUserInput = () => {
-    const input = document.createElement("input")
-
-    input.addEventListener("input", this.inputEvent)
-    
-    this.main.appendChild(input)
-
-    // TODO
-    // automagically put the user into the input
-  }
-
-  inputEvent = e => {
-    console.log("some event")
-    // TODO
-    // change the LoC styling
-    // do patch request
-    // check to see if the game is over
-    // a lot more stuff...
-    this.user.score.updateScore()
+    this.userInput = new UserInput(this.main, this.user)
   }
 }
