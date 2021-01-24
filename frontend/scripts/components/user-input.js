fd308c5364b3666fc5aa8996eec3e2a0ad93d850
Igor Veyner <igorveyner95@gmail.com>
2021-01-24 11:46:07 -0500

    move logic of creating elements and adding events to component file

--- frontend/scripts/components/user-input.js
+++ frontend/scripts/components/user-input.js
@@ -2,5 +2,25 @@ class UserInput {
   constructor(main, user) {
     this.main = main
     this.user = user
+    this.addUserInput()
+  }
+
+  addUserInput = () => {
+    const input = document.createElement("input")
+    input.addEventListener("input", this.inputEvent)
+    this.main.appendChild(input)
+
+    // TODO
+    // automagically put the user into the input
+  }
+
+  inputEvent = e => {
+    console.log("some event")
+    // TODO
+    // change the LoC styling
+    // do patch request
+    // check to see if the game is over
+    // a lot more stuff...
+    this.user.score.updateScore()
   }
 }
