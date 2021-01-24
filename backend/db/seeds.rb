8ad6203f0f0dd9abc9922a115f8b28c78707ea05
Igor Veyner <igorveyner95@gmail.com>
2021-01-24 15:24:32 -0500

    update seed file to have small code for quick testing

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,5 +2,6 @@ User.destroy_all
 Score.destroy_all
 Code.destroy_all
 
-Code.create(name: "Map", line: "a.map.with_index {|x,i| x * i}")
-Code.create(name: "Compact", line: "[\"a\", nil, \"b\", nil, \"c\", nil].compact")
+Code.create(name: "", line: "a")
+# Code.create(name: "Map", line: "a.map.with_index {|x,i| x * i}")
+# Code.create(name: "Compact", line: "[\"a\", nil, \"b\", nil, \"c\", nil].compact")
