5551945f9b53272ccac2edc6681e7abc5b8edd16
Igor Veyner <igorveyner95@gmail.com>
2021-01-25 16:40:07 -0500

    update comment

--- frontend/scripts/models/code.js
+++ frontend/scripts/models/code.js
@@ -15,7 +15,8 @@ class Code {
 
     // Create Score needs to run after the above data is assigned,
     //  otherwise score creation will not have a code_id to send
-    //  and it does not get created in the DB
+    //  and it does not get created in the DB.
+    //  Putting it outside the then statement will make it run before the assignment
 
     user.createScore()
     user.createGameDisplay()
