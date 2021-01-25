3844a7d2555dfad18270f788b2dff832f1abfcbe
Igor Veyner <igorveyner95@gmail.com>
2021-01-25 17:56:18 -0500

    space index.css and add spacing to button containers

--- frontend/scripts/components/user-input.js
+++ frontend/scripts/components/user-input.js
@@ -36,7 +36,7 @@ class UserInput {
   createButtons = () => {
     const elementsToCreate = ["div", "div", "h3", "h3", "p", "p", "button", "button"]
     const [btn1Container, btn2Container, header1, header2, info1, info2, tryAgain, newRandom] = elementsToCreate.map(element => document.createElement(element))
-    
+
     header1.innerText = "Try Again"
     header2.innerText = "Random"
 
@@ -53,7 +53,10 @@ class UserInput {
     tryAgain.addEventListener("click", this.tryAgain)
     newRandom.addEventListener("click", this.newRandom)
 
-    for(const element of [btn1Container, btn2Container]) { main.appendChild(element) }
+    for(const element of [btn1Container, btn2Container]) { 
+      element.classList.add("button-container")
+      main.appendChild(element) 
+    }
 
     window.addEventListener("keydown", this.keydownHandler)
   }
