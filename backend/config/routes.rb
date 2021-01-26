68e0886b1812a0477e8b0be937e509153c3ce8ff
Igor Veyner <igorveyner95@gmail.com>
2021-01-26 18:46:13 -0500

    remove unneeded route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/codes/random', to: 'codes#random', as: 'random'
   resources :scores, only: [:create, :update, :show]
   resources :users, only: [:create]
-  resources :highscores, only: [:create, :update, :show]
+  resources :highscores, only: [:create, :update]
 end
