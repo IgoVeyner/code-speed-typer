e80a8e9886ed25735cba32f49b0467b98d94f0fe
Igor Veyner <igorveyner95@gmail.com>
2021-01-26 17:54:39 -0500

    create update action and high highscore helper

--- backend/app/controllers/highscores_controller.rb
+++ backend/app/controllers/highscores_controller.rb
@@ -4,7 +4,23 @@ class HighscoresController < ApplicationController
     render json: HighscoreSerializer.new(highscore)
   end
 
+  def update 
+    if @highscore
+      if @highscore.update(highscore_params)
+        render json: HighscoreSerializer.new(@highscore)
+      else
+        render json: {error: @highscore.errors.full_messages, status: 400}, status: 400
+      end
+    else
+      render json: {error: "Sorry, there is no highscore with that ID", status: 400}, status: 400
+    end
+  end
+
   private
+
+  def find_highscore
+    @highscore = Highscore.find_by_id(params[:id])
+  end
 
   def highscore_params
     params.require(:highscore).permit(:score_id, :code_id)
