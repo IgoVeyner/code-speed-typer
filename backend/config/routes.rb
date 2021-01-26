a70da334ff875961e5a8b2795da0ac1dca3427ea
Igor Veyner <igorveyner95@gmail.com>
2021-01-26 14:18:42 -0500

    add highscore routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :highscores
   get '/codes/random', to: 'codes#random', as: 'random'
   resources :codes, only: [:show]
   resources :scores, only: [:create, :update, :show]
   resources :users, only: [:create]
+  resources :highscores, only: [:create, :update, :show]
 end
