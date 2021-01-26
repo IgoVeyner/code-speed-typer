b79b48034a1a0e26c066955cc4f331540c8e7156
Igor Veyner <igorveyner95@gmail.com>
2021-01-26 16:04:39 -0500

    add create action to highscores controller and private strict params helper

--- backend/app/controllers/highscores_controller.rb
+++ backend/app/controllers/highscores_controller.rb
@@ -1,2 +1,12 @@
 class HighscoresController < ApplicationController
+  def create
+    highscore = Highscore.create(highscore_params)
+    render json: HighscoreSerializer(highscore)
+  end
+
+  private
+
+  def highscore_params
+    params.require(:highscore).permit(:score_id, :code_id)
+  end
 end
