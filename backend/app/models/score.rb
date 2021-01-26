b5f222abf88dd5e50a92eb188060703ba5a967b5
Igor Veyner <igorveyner95@gmail.com>
2021-01-26 14:10:49 -0500

    add validations to score model

--- backend/app/models/score.rb
+++ backend/app/models/score.rb
@@ -1,4 +1,7 @@
 class Score < ApplicationRecord
   belongs_to :code
   belongs_to :user
+
+  validates :time, numericality: { greater_than_or_equal_to: 0 }
+  validates :strikes, numericality: { greater_than_or_equal_to: 0 }
 end
