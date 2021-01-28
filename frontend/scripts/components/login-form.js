06e33017e59447b5d26765feae4708a1061a27c0
Igor Veyner <igorveyner95@gmail.com>
2021-01-28 15:27:44 -0500

    style button and change text to start

--- frontend/scripts/components/login-form.js
+++ frontend/scripts/components/login-form.js
@@ -9,13 +9,14 @@ class LoginForm {
 
     header.innerText = "Enter a Username"
     submit.type = "submit"
-    submit.value = "Continue"
+    submit.value = "Start"
 
     form.addEventListener("submit", this.postToUsers)
 
     form.classList.add("login-form")
     header.classList.add("form-header")
     input.classList.add('login-input')
+    submit.classList.add('login-submit')
 
     for (const element of [header, input, submit]) { form.appendChild(element) }
     main.appendChild(form)
