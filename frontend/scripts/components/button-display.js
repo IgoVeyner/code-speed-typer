612f049d925f2fbe448386869b3d0c7406242c50
Igor Veyner <igorveyner95@gmail.com>
2021-01-29 15:47:00 -0500

    remove old func

--- frontend/scripts/components/button-display.js
+++ frontend/scripts/components/button-display.js
@@ -1,37 +1,9 @@
 class ButtonDisplay {
   constructor() {
-    this.createButtonsTemp()
+    this.createButtons()
   }
 
   createButtons = () => {
-    const elementsToCreate = ["div", "div", "h3", "h3", "p", "p", "button", "button"]
-    const [btn1Container, btn2Container, header1, header2, info1, info2, tryAgain, newRandom] = elementsToCreate.map(element => document.createElement(element))
-
-    header1.innerText = "Try Again"
-    header2.innerText = "Random"
-
-    info1.innerText = "(backspace)"
-    info2.innerText = "(enter)"
-
-    tryAgain.style.background = "url(./assets/backspace.png)"
-    newRandom.style.background = "url(./assets/enter.png)"
-
-    for(const element of [tryAgain, newRandom]) { element.classList.add("button") }
-    for(const element of [header1, tryAgain, info1]) { btn1Container.appendChild(element)}
-    for(const element of [header2, newRandom, info2]) { btn2Container.appendChild(element)}
-
-    tryAgain.addEventListener("click", () => { this.removeKeydownHandler(); user.resetGame() })
-    newRandom.addEventListener("click", () => { this.removeKeydownHandler(); user.newRandomGame() })
-
-    for(const element of [btn1Container, btn2Container]) { 
-      element.classList.add("button-container")
-      main.appendChild(element) 
-    }
-
-    window.addEventListener("keydown", this.keydownHandler)
-  }
-
-  createButtonsTemp = () => {
     const [buttonsContainer, btn1Container, btn2Container] = this.createButtonContainers()
 
     let i = 0
