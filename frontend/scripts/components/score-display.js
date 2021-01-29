bdc615e3b4361e90402771d876107dfa826971ca
Igor Veyner <igorveyner95@gmail.com>
2021-01-29 17:54:28 -0500

    refactor code from createFromScore and add classes

--- frontend/scripts/components/score-display.js
+++ frontend/scripts/components/score-display.js
@@ -1,6 +1,7 @@
 class ScoreDisplay {
   constructor(score) {
-    score ? this.createFromScore(score) : this.createDisplay()
+    // score ? this.createFromScore(score) : this.createDisplay()
+    score ? this.createFromScoreTemp(score) : this.createDisplay()
   }
 
   createDisplay = () => {
@@ -53,8 +54,20 @@ class ScoreDisplay {
   }
 
   createFromScoreTemp = score => {
-    const [container, header, text] = this.createElements(["div", "h1", "h2"])
+    const [container, header, text, timer] = this.createElements(["div", "h1", "h2", "span"])
+    let time = `${score.time / 10}`
+    this.timer = timer
+    this.timerConverter(time)
+
+    header.innerText = "Highscore"
+    text.innerText = `Name: ${score.username} | Time: ${this.timer.innerText} | Strikes: ${score.strikes}`
     
+    header.classList.add("highscore-header")
+    text.classList.add("highscore-text")
+    container.classList.add("highscore-container")
+
+    for (const element of [header, text]) { container.appendChild(element) }
+    user.display.codeDisplay.characters.appendChild(container)
   }
 
   add_username = (username, div) => {
