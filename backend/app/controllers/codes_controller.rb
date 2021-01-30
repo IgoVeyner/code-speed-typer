c6240c8a03e78af3fde4ab22e405906aec123e2f
Igor Veyner <igorveyner95@gmail.com>
2021-01-30 13:20:01 -0500

    remove private helper and abstract finding random code to code class and update to local variable from instance variable

--- backend/app/controllers/codes_controller.rb
+++ backend/app/controllers/codes_controller.rb
@@ -1,22 +1,12 @@
 class CodesController < ApplicationController
-  before_action :find_code, only: [:show]
-
   def random 
-    count = Code.count
-    num = rand(count) + 1
-    @code = find_code(num)
+    code = Code.get_random()
 
-    if @code
+    if code
       options = { include: [:highscores] }
-      render json: CodeSerializer.new(@code, options)
+      render json: CodeSerializer.new(code, options)
     else
       render json: {error: "Sorry, there is no code with that ID", status: 400}, status: 400
     end
-  end
-
-  private
-
-  def find_code(id)
-    @code = Code.find_by_id(id)
   end
 end
