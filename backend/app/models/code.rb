7004be1e9c7ea299d7718064f9ab9693fd44f8a1
Igor Veyner <igorveyner95@gmail.com>
2021-01-30 13:19:23 -0500

    add class method to code to find a random code from DB

--- backend/app/models/code.rb
+++ backend/app/models/code.rb
@@ -2,4 +2,10 @@ class Code < ApplicationRecord
   has_many :scores
   has_many :users, through: :scores
   has_many :highscores
+
+  def self.get_random
+    count = Code.count
+    num = rand(count) + 1
+    Code.find_by_id(num)
+  end
 end
