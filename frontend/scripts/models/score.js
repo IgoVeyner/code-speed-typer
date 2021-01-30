decfee19d67bfd0c05be8de67e642355ed501a68
Igor Veyner <igorveyner95@gmail.com>
2021-01-30 16:56:36 -0500

    move logic of updating score instance's attributes all to getCurrentScore helper and remove weird code that set it from return value

--- frontend/scripts/models/score.js
+++ frontend/scripts/models/score.js
@@ -9,8 +9,7 @@ class Score {
   createScore = () => api.postScore(user.id, user.code.id).then(this.assignID) 
   
   updateScore = () => {
-    this.completed = true
-    [this.time, this.strikes] = this.getCurrentScore()
+    this.getCurrentScore()
 
     api.updateScore(this.id, this.time, this.strikes, this.completed)
     .then(data => {
@@ -39,13 +38,13 @@ class Score {
   }
 
   getCurrentScore = () => {
-    let timer = user.display.scoreDiv.timer.innerText
-    let strikes = user.display.scoreDiv.num.innerText
+    this.completed = true
+    const timer = user.display.scoreDiv.timer.innerText
+    const strikes = user.display.scoreDiv.num.innerText
 
-    timer = parseInt(timer.replace(":", ""), 10) * 10
-    strikes = parseInt(strikes)    
-
-    return [timer, strikes]
+    this.time = parseInt(timer.replace(":", ""), 10) * 10
+    this.strikes = parseInt(strikes)    
+    console.log(this)
   }
 
   assignID = scoreData => this.id = scoreData.id 
