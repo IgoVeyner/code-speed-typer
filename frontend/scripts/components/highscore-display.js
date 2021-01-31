61570b8532165ebe7fe5f3efbe1c2cfecd737663
Igor Veyner <igorveyner95@gmail.com>
2021-01-31 13:04:09 -0500

    move createElements and timeConverter funcs to highscore display

--- frontend/scripts/components/highscore-display.js
+++ frontend/scripts/components/highscore-display.js
@@ -19,4 +19,20 @@ class HighscoreDisplay {
     user.display.codeDisplay.container.appendChild(container)
   }
 
+  createElements = ([...elements]) => { return elements.map(element => document.createElement(element)) }
+
+  timerConverter = time => {
+    if (time.length < 2) {
+      this.timer.innerText = `00:0${time}`
+    } else if (time.length < 3) {
+      this.timer.innerText = `00:${time}`
+    } else if (time.length < 5) {
+      let seconds = time.slice(0, time.length - 2)
+      if (seconds.length == 1) { seconds = `0${seconds}`} 
+      this.timer.innerText = seconds + ":" + time.slice(time.length - 2, time.length)
+    } else {
+      if (this.interval) { this.stopTimer() }
+      this.timer.innerText == "99:99"
+    }
+  }
 }
