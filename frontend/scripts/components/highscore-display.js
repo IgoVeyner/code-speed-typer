1ae28fa6b842ae5785ab3e42c31702b9606ffc2b
Igor Veyner <igorveyner95@gmail.com>
2021-01-31 13:03:30 -0500

    move function from score-display to highscore-display

--- frontend/scripts/components/highscore-display.js
+++ frontend/scripts/components/highscore-display.js
@@ -1,5 +1,22 @@
 class HighscoreDisplay {
   constructor() {
-    
   }
+
+  static createFromScore = score => {
+    const [container, header, text, timer] = this.createElements(["div", "h1", "h2", "span"])
+    let time = `${score.time / 10}`
+    this.timer = timer
+    this.timerConverter(time)
+
+    header.innerText = "Highscore"
+    text.innerText = `Name: ${score.username} | Time: ${this.timer.innerText} | Strikes: ${score.strikes}`
+    
+    header.classList.add("highscore-header")
+    text.classList.add("highscore-text")
+    container.classList.add("highscore-container")
+
+    for (const element of [header, text]) { container.appendChild(element) }
+    user.display.codeDisplay.container.appendChild(container)
+  }
+
 }
