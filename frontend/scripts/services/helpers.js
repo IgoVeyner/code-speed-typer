cbd4c604e8b759c56d1abc2752eddaf6b994f58e
Igor Veyner <igorveyner95@gmail.com>
2021-01-31 13:57:10 -0500

    adjust timerConverter to accept the argument of the instance its mutating

--- frontend/scripts/services/helpers.js
+++ frontend/scripts/services/helpers.js
@@ -2,18 +2,18 @@ class Helpers {
   constructor() {
   }
 
-  timerConverter = time => {
+  timerConverter = (display, time) => {
     if (time.length < 2) {
-      this.timer.innerText = `00:0${time}`
+      display.timer.innerText = `00:0${time}`
     } else if (time.length < 3) {
-      this.timer.innerText = `00:${time}`
+      display.timer.innerText = `00:${time}`
     } else if (time.length < 5) {
       let seconds = time.slice(0, time.length - 2)
       if (seconds.length == 1) { seconds = `0${seconds}`} 
-      this.timer.innerText = seconds + ":" + time.slice(time.length - 2, time.length)
+      display.timer.innerText = seconds + ":" + time.slice(time.length - 2, time.length)
     } else {
-      if (this.interval) { this.stopTimer() }
-      this.timer.innerText == "99:99"
+      if (display.interval) { display.stopTimer() }
+      display.timer.innerText == "99:99"
     }
   }
   
